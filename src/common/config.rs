08b169f290b26edd0242fa9672d6d382fc66240f
fgh1999 <fanguanghong1999@163.com>
2020-11-13 13:14:09 +0800

    [fmt] cargo fmt

--- src/common/config.rs
+++ src/common/config.rs
@@ -1,17 +1,17 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use std::fs::File;
 
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
 pub struct SecurityPath {
     pub sk_path: String,
     pub pk_path: String,
-    pub ca_path: String
+    pub ca_path: String,
 }
 
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
 pub struct ServerConfig {
     pub addr: String,
-    pub keys: SecurityPath
+    pub keys: SecurityPath,
 }
 
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
@@ -24,24 +24,24 @@ pub struct ClientAddrs {
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
 pub struct ClientConfig {
     pub addrs: ClientAddrs,
-    pub keys: SecurityPath
+    pub keys: SecurityPath,
 }
 
 enum ConfigPath {
     Server(String),
-    Client(String)
+    Client(String),
 }
 
 enum Config {
     Server(ServerConfig),
-    Client(ClientConfig)
+    Client(ClientConfig),
 }
 
 #[allow(dead_code)]
 pub fn load_client_config(path: &str) -> ClientConfig {
     match load_and_parse_config(ConfigPath::Client(String::from(path))) {
         Config::Client(config) => config,
-        _ => panic!(format!("{} does not point to a server config", &path))
+        _ => panic!(format!("{} does not point to a server config", &path)),
     }
 }
 
@@ -49,7 +49,7 @@ pub fn load_client_config(path: &str) -> ClientConfig {
 pub fn load_server_config(path: &str) -> ServerConfig {
     match load_and_parse_config(ConfigPath::Server(String::from(path))) {
         Config::Server(config) => config,
-        _ => panic!(format!("{} does not point to a client config", path))
+        _ => panic!(format!("{} does not point to a client config", path)),
     }
 }
 
